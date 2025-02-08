7b874bf5a91f0db01f4858fab664a34b4f6b5708
Shahroz Ahmad <97090106+ishahroz@users.noreply.github.com>
2025-02-08 18:01:50 -0400

    feat: add daphne and django channels in backend (#3)

--- backend/core/asgi.py
+++ backend/core/asgi.py
@@ -1,5 +1,4 @@
-"""
-ASGI config for core project.
+"""ASGI config for core project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -7,10 +6,19 @@ For more information on this file, see
 https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
 """
 
-import os
-
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
+from chat.routing import websocket_urlpatterns as chat_ws_urlpatterns
 
-application = get_asgi_application()
+django_asgi_app = get_asgi_application()
+
+application = ProtocolTypeRouter(
+    {
+        "http": django_asgi_app,  # handle HTTP requests
+        "websocket": AuthMiddlewareStack(
+            URLRouter(chat_ws_urlpatterns),
+        ),  # handle WebSocket requests
+    },
+)
